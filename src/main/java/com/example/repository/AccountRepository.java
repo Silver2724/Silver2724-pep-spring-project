a5d778b7b5158ca8b09699b3e0de6cab8dd7600c
Diya Bhandarkar <diya2724@gmail.com>
2025-04-12 22:53:36 +0000

    had to recommit because project wasn't accepting the submit

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Integer>{
     //looks at Account table and finds the username that equals the given username
-    //it returns the account that matches the given username
+    //it will then returns the account that matches the given username
     Optional<Account> findByUsername(String username);
     
     //tells if there's an account in the database with the given username
